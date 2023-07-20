bddf221f27749ee0f824ca202c58583e35032dea
Wesley Vicente <wesley@maismei.com.br>
2023-07-20 17:17:07 -0300

    start home and movie card component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
-export default Home
+import "../styles/pages/home.scss"
+
+const moviesAPI = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+
+    const [topMovies, setTopMovies] = useState([]);
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
+    }
+
+    useEffect(() => {
+        
+        const topRatedUrl = `${moviesAPI}top_rated?${apiKey}`;
+
+        getTopRatedMovies(topRatedUrl);
+
+    }, [])
+
+    return (
+        <main className="main-container">
+            <div className="container-movies">
+                {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/> )}
+            </div>
+        </main>
+    )
+};
+
+export default Home;
